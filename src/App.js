ea22cd738d53cc43b32bde642abf95da670d0b4c
konok101 <rockykonok@gmail.com>
2022-06-04 00:08:14 +0600

    d

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
 
   const [email, setEmail]=useState('');
   const [password, setPassword]= useState('');
+  const [validated, setValidated] = useState(false);
+
 
   const HandleEmailBlur = (event) => {
      setEmail(event.target.value)
@@ -24,6 +26,16 @@ function App() {
   }
 
   const handleFormSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+
+  return; 
+       
+    }
+    setValidated(true);
+
     createUserWithEmailAndPassword(auth, email, password)
     .then(result =>{
       const user = result.user;
@@ -39,18 +51,24 @@ function App() {
     <div  >
       <div className='mx-auto w-25'>
         <h2>Please registration</h2>
-        <Form onSubmit={handleFormSubmit}>
+        <Form noValidate validated={validated}  onSubmit={handleFormSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control onBlur={HandleEmailBlur} type="email" placeholder="Enter email" required />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
+            <Form.Control.Feedback type="invalid">
+            Please provide a valid city.
+          </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control onBlur={handlePassBlur} type="password" placeholder="Password" required />
+            <Form.Control.Feedback type="invalid">
+            Please provide a valid city.
+          </Form.Control.Feedback>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
